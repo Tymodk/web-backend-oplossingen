4d3eb55d05594e3d48122cea0d55456f06d6c5ac
tymo.dekock <tymo.dekock>
2016-10-06 15:49:34 +0200

    Oplossing toegevoegd

---
+++ Opdracht 1/opdracht-comments-oplossing.php
@@ -0,0 +1,12 @@
+<?php 
+	/* Tymo de Kock
+	Tymo.dekock@student.kdg.be*/
+?>
+
+<!DOCTYPE HTML5> 
+<html>
+	<head><title> Opdract 1 - Commentes - Oplossing</title></head>
+	<h1>Echo of name: </h1>
+	<?php echo 'Tymo de Kock'?>
+
+</html> 
