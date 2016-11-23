1463a4c50e188ecb1d2bf99cfe1e1c662807aabc
tymo.dekock <tymo.dekock>
2016-11-23 13:19:32 +0100

    oplossing toevoegen

---
+++ Opdracht 22 - Include Require/opdracht-includerequire-oplossing.php
@@ -0,0 +1,15 @@
+<?php
+	$artikels =	array(	array('title'=> 'Titel artikel 1',
+							  'body'=> 'Body artikel 1',
+							  'tags'=> 'Kernwoorden artikel 1',),
+						array('title'=> 'Titel artikel 2',
+							  'body'=> 'Body artikel 2',
+							  'tags'=> 'Kernwoorden artikel 2',),
+					    array('title'=> 'Titel artikel 3',
+							  'body'=> 'Body artikel 3',
+							  'tags'=> 'Kernwoorden artikel 3',));
+	include 'views/header-partial.html';
+	include 'views/body-partial.html';
+	include 'views/footer-partial.html';
+?>
+
