96a7fa98d8b7d819c577b56989f9b44a2642da74
tymo.dekock <tymo.dekock>
2016-12-13 17:21:19 +0100

    Deel oplossing toegevoegd

---
+++ Opdracht 30 - Security Login/registratie-form.php
@@ -0,0 +1,49 @@
+<?php
+  session_start();
+  function __autoload( $classname ){
+    require_once( $classname . '.php' );
+  }
+  $email = ''; // to not get errors from form requiring these vars
+  $password = ''; // ^
+  $errors = '';
+  if(isset($_SESSION['errorLog'])){
+  $errors = $_SESSION['errorLog'];  
+  }
+  if(isset($_SESSION['registerInfo'])){
+    $email = $_SESSION['registerInfo']['email'];
+    $password = $_SESSION['registerInfo']['password'];
+  }
+  
+?>
+
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Security</title>
+    
+	</head>
+	<body>
+    <pre><?= var_dump($_SESSION) ?></pre>
+    <pre><?= var_dump($_COOKIE) ?></pre>
+   <?php if($errors): ?>
+      <?php foreach ($errors as $error) ?>        
+      <p><?= $error ?></p>
+   <?php endif ?>
+   <form action="registratie-process.php" method="post">
+      <ul>
+        <li>
+          <p>Email<p>
+          <input type="text" name="email" id="email" value= "<?= $email ?>">
+        </li>
+        
+        <li>
+          <p>Paswoord</p>
+          <input type="<?= ($password !='')? 'text' : 'password' ?>" name="password" id="password" value= "<?= $password ?>">
+          <input type="submit" name="generatePassword" value="genereer een paswoord">
+        </li>
+      </ul>
+      
+      <input type="submit" name="submit" value="log in">
+    </form>
+	</body>
+</html>
